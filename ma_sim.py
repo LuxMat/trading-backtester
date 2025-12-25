e6b7194f2690a5f56d2753b36d65248b6feefb83
LuxMat <12345678+LuxM._at@users.noreply.github.com>
2025-12-25 11:53:31 +0100

    ma_simulator

---
+++ ma_sim.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import utils
+import instrument
+pd.set_option('display.max_columns', None) #show all columns when printing dataframe
+
+
+def is_trade(row):
+    if row.DIFF >= 0 and row.DIFF_PREV < 0:
+        return 1  # Buy
+    elif row.DIFF < 0 and row.DIFF_PREV >= 0:
+        return -1  # Sell
+    else:
+        return 0  # No trade
+
+
+def get_ma_col(ma):
+    return f"MA_{ma}"
+
+
+def evaluate_pair(i_pair, ma_short, ma_long, price_data):
+
+    price_data['DIFF'] = price_data[get_ma_col(ma_short)] - price_data[get_ma_col(ma_long)]
+    price_data['DIFF_PREV'] = price_data['DIFF'].shift(1)
+    price_data['IS_TRADE'] = price_data.apply(is_trade, axis=1)
+
+    df_trades = price_data[price_data['IS_TRADE'] != 0].copy()
+    #df_trades['DELTA'] = (df_trades.close.diff()/i_pair.pipLocation).shift(-1)  #calculate difference between trade prices, shifted by 1 to avoid lookahead bias
+    df_trades['DELTA'] = df_trades.close.diff().shift(-1)
+    df_trades['GAINS'] = df_trades["DELTA"] * df_trades["IS_TRADE"]  #calculate gains based on trade direction
+
+
+    #print(f'{i_pair.pairname} MA Short: {ma_short}, MA Long: {ma_long}, Trades:{df_trades.shape[0]}, Total Gains: {df_trades["Gains"].sum():.0f}')
+    print(f'USD_BCT, MA Short: {ma_short}, MA Long: {ma_long}, Trades:{df_trades.shape[0]}, Total Gains: {df_trades["GAINS"].sum():.0f}')
+
+    return df_trades['GAINS'].sum()
+
+
+
+def get_price_data(pairname, granularity):
+    df = pd.read_csv(utils.get_his_data_filename(pairname, granularity))
+    df[["open_time", "close_time"]] = df[["open_time", "close_time"]].apply(pd.to_datetime, utc=True)            #to be deleted...
+    non_cols = ['open_time', 'volume']
+    mod_cols = [x for x in df.columns if x not in non_cols]
+    df[mod_cols] = df[mod_cols].apply(pd.to_numeric)
+
+    return df[['open_time', 'close']]
+
+
+
+def processs_data(ma_short, ma_long, price_data):
+    ma_list = set(ma_short + ma_long)
+
+    for ma in ma_list:
+        price_data[get_ma_col(ma)] = price_data['close'].rolling(window=ma).mean()
+
+    return price_data
+
+
+
+def run():
+    pairname = "BTC_USD"
+    granularity = "1m"
+    ma_short = [8, 10, 12]
+    ma_long = [21, 34, 55]
+    #i_pair = instrument.Instrument(pairname)
+
+    price_data = get_price_data(pairname, granularity)
+    price_data = processs_data(ma_short, ma_long, price_data)
+    print(price_data.tail())
+
+    #best_gain = None
+    best = -100000.0    #arbitrary large negative number, best_gain
+    b_mashort = 0           #best ma_short
+    b_malong = 0        #best ma_long
+
+
+    #iterate through all combinations of ma_short and ma_long to find the best performing pair.
+    for _malong in ma_long:
+        for _mashort in ma_short:
+            if _mashort >= _malong:
+                continue
+            i_pair_holder = None  #dummy for now
+            res = evaluate_pair(i_pair_holder, _mashort, _malong, price_data.copy()) #result
+ 
+            if res > best:
+                best = res
+                b_mashort = _mashort
+                b_malong = _malong
+
+    print(f'Best MA Short: {b_mashort}, Best MA Long: {b_malong}, Best Gains: {best:.0f}')
+
+
+if __name__ == "__main__":
+    run()
